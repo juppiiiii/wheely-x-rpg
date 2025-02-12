fc4afd9bcee7c0af362708616f2215fc6da713e7
김한민 <kdg5794@gmail.com>
2025-02-12 15:53:56 +0900

    버튼하이라이트, 키보드조작, 효과음찾는중

---
+++ Assets/Script/backGroundMusic.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class backGroundMusic : MonoBehaviour
+{
+    public AudioSource bgmSource;
+    public AudioClip bgmClip;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (bgmSource != null && bgmClip != null)
+        {
+            bgmSource.clip = bgmClip;
+            bgmSource.loop = true; // BGM 반복 재생
+            bgmSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
